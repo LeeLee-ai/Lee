c6d0e03e65a25ca1b9ef8b61767df1f193a1f1d6
LeeLee-ai <80662714+LeeLee-ai@users.noreply.github.com>
2021-03-15 13:09:54 +0900

    2주자 배운거

---
+++ indexing.py
@@ -0,0 +1,10 @@
+word = 'python'
+print (word[0])
+변수1 = input('첫번 째 단어를 입력해주세요:')
+변수2 = input('두번째 단어를 입력해주세요:')
+변수3= input('세번째 단어를 입력해주세요:')
+print(변수1[0]+변수2[0]+변수3[0])  
+shopping_list = ['milk', 'eggs', 'choose', 'butter', 'cream']
+print(shopping_list[0])
+shopping_list[2] = 'apple'
+print(shopping_list)
